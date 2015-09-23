edde0f46e5cb63930dade6a6b02e6f7ea1c0aff6
Christina Wolfe <cmw0513@gmail.com>
2015-09-23 14:59:38 +0200

    added redirections for invalid forms

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,9 +6,12 @@ class ReviewsController < ApplicationController
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @restaurant.reviews.create(review_params)
-
-    redirect_to restaurant_path(@restaurant)
+    @review = @restaurant.reviews.new(review_params)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
